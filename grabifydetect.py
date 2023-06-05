a9bed64e173f7e54f94195bb6e31d9d51f16be65
sheikhjamal <77934521+sheikhjamal@users.noreply.github.com>
2023-06-05 21:06:05 +0300

    Update on GrabifyDetect
    Just wanted to add GUI and domain list update

--- grabifydetect.py
+++ grabifydetect.py
@@ -1,11 +1,50 @@
-from urllib.parse import urlparse 
-linklist = ("lovebird.guru", "trulove.guru", "dateing.club", "shrekis.life", "headshot.monster", "gaming-at-my.best", "programing.monster", "yourmy.monster", "imageshare.best", "screenshot.best", "gamingfun.me", "catsnthings.com", "catsnthings.fun", "curiouscat.club", "joinmy.site", "fortnitechat.site", "fortnight.space", "freegiftcards.co", "stopify.co", "leancoding.co", "grabify.link", "photovaults.pics", "bathtub.pics", "foot.wiki", "thisdomainislong.lol","gamergirl.pro","picshost.pics", "pichost.pics", "gamertag.shop","imghost.pics","imagehost.pics","toldyouso.lol","toldyouso.pics","screenshare.pics", "myprivate.pics", "noodshare.pics", "cheapcinema.club", "cheapcinema.club", "shhh.lol","partpicker.shop","sportshub.var","locations.quest")
+from urllib.parse import urlparse
+import tkinter as tk
+from tkinter import ttk
 
-print("GrabifyDetect by sheikhjamal")
-while True:
-  link = input("Enter the link: ")
-  domain = urlparse(link).netloc
-  if domain in linklist:
-    print(link + " is from Grabify")
-  else:
-    print(link + " is not from Grabify")
+linklist = (
+    "lovebird.guru", "trulove.guru", "dateing.club", "shrekis.life",
+    "headshot.monster", "gaming-at-my.best", "programming.monster",
+    "yourmy.monster", "imageshare.best", "screenshot.best",
+    "gamingfun.me", "catsnthings.com", "catsnthings.fun",
+    "curiouscat.club", "joinmy.site", "fortnitechat.site",
+    "fortnight.space", "freegiftcards.co", "stopify.co",
+    "leancoding.co", "grabify.link", "photovaults.pics",
+    "bathtub.pics", "foot.wiki", "thisdomainislong.lol",
+    "gamergirl.pro", "picshost.pics", "pichost.pics",
+    "gamertag.shop", "imghost.pics", "imagehost.pics",
+    "toldyouso.lol", "toldyouso.pics", "screenshare.pics",
+    "myprivate.pics", "noodshare.pics", "cheapcinema.club",
+    "shhh.lol", "partpicker.shop", "sportshub.var", "locations.quest"
+)
+
+
+def check_link():
+    link = entry.get()
+    domain = urlparse(link).netloc.lower()  # Convert to lowercase for case-insensitive comparison
+    if domain in linklist:
+        result_label.config(text=link + " is not a Grabify link")
+    else:
+        result_label.config(text=link + " is a Grabify link")
+
+
+window = tk.Tk()
+window.title("Grabify Detector")
+window.geometry("400x250")  # Set the window size
+
+title_label = ttk.Label(window, text="Grabify Detector", font=("Arial", 16))
+title_label.pack(pady=20)
+
+entry = ttk.Entry(window, width=30)
+entry.pack(pady=10)
+
+button = ttk.Button(window, text="Check", command=check_link)
+button.pack()
+
+result_label = ttk.Label(window, text="")
+result_label.pack(pady=10)
+
+credit_label = ttk.Label(window, text="Made by sheikhjamal", font=("Arial", 10))
+credit_label.pack(pady=5)
+
+window.mainloop()
